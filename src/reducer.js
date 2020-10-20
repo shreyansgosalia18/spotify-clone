96a6d3cb0af9b110d6d362c76e8a33cd80b6102a
shreyansgosalia18 <shreyansgosalia18@gmail.com>
2020-10-20 17:06:55 +0530

    first commit

---
+++ src/reducer.js
@@ -0,0 +1,72 @@
+export const initialState = {
+    user: null,
+    playlists: [],
+    spotify: null,
+    discover_weekly: null,
+    top_artists: null,
+    playing: false,
+    item: null,
+
+    // REMOVE AFTER COMPLETION
+    //token: 'BQDeWVQxBvKK1RM7lO1tvGk66YGHZ3qvPSxQhtYzg8PUEPOULZs8xVIS9vg441pGnKyGtN7HABlhmTqYBy4OcDXB2VndhMb9DEJWDSh-B2619YED69bXWQPoAMY2hlpmDZHQUdc2gj8j1wqVh9wIsNkvwgNwhc1rDCTB51KXexh9ASdmgzn7',
+};
+
+const reducer = (state,action) => {
+    console.log(action);
+
+    //Action -> type, [payload]
+    switch(action.type){
+        case 'SET_USER':
+            return{
+                ...state,
+                user: action.user
+            }
+        
+        case "SET_TOKEN":
+            return {
+                ...state,
+                token: action.token,
+            };
+        
+        case "SET_PLAYLISTS":
+            return {
+                ...state,
+                playlists: action.playlists,
+            };
+        
+        case "SET_DISCOVER_WEEKLY":
+            return {
+                ...state,
+                discover_weekly: action.discover_weekly,
+            };
+            
+        case "SET_PLAYING":
+            return {
+                ...state,
+                playing: action.playing,
+            };
+        
+        case "SET_ITEM":
+            return {
+                ...state,
+                item: action.item,
+            };
+
+        case "SET_TOP_ARTISTS":
+            return {
+                ...state,
+                top_artists: action.top_artists,
+            };    
+
+        case "SET_SPOTIFY":
+            return {
+                ...state,
+                spotify: action.spotify,
+            };    
+
+        default:
+            return state;
+    }
+}
+
+export default reducer;
